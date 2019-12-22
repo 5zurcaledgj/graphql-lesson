e748fa24e9b01105c124409cdc29690695020e82
Joshua dela Cruz <joshua@ecommercewebsites.com.au>
2019-12-22 18:27:59 +0800

    added the mutation to add cart items to local cache and display them to drop down

--- src/graphql/resolver.js
+++ src/graphql/resolver.js
@@ -1,14 +1,27 @@
 import { gql } from "apollo-boost";
 
+import { addItemToCart } from "./cart.utils.js";
+
 export const typeDef = gql`
+  extend type Item {
+    quantity: Int
+  }
+
   extend type Mutation {
     ToggleCartHidden: Boolean!
+    AddItemToCart(item: Item!): [Item]!
   }
 `;
 
 const GET_CART_HIDDEN = gql`
   {
     cartHidden @client
+  }
+`;
+
+const GET_CART_ITEMS = gql`
+  {
+    cartItems @client
   }
 `;
 
@@ -26,6 +39,22 @@ export const resolvers = {
       });
 
       return !cartHidden;
+    },
+    addItemToCart: (_root, _args, _context, _info) => {
+      const { cache } = _context;
+      const { item } = _args;
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS
+      });
+
+      const updatedCartItems = addItemToCart(cartItems, item);
+
+      cache.writeQuery({
+        query: GET_CART_ITEMS,
+        data: { cartItems: updatedCartItems }
+      });
+
+      return updatedCartItems;
     }
   }
 };
